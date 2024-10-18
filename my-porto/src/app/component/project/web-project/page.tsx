5b25bb57d52718b67a3f14b3f514a8943055dd88
Frisqia <frifani123@gmail.com>
2024-10-18 20:40:31 +0700

    bukti project

--- my-porto/src/app/component/project/web-project/page.tsx
+++ my-porto/src/app/component/project/web-project/page.tsx
@@ -21,19 +21,19 @@ export default function MyPortofolio() {
   
   return (
     <>
-      <div className="flex flex-col justify-center items-center p-6 md:p-8 lg:p-10 bg-pink-500 text-white min-h-screen">
-        <h1 className="text-base md:text-xl lg:text-2xl">My Portfolio</h1>
+      <div className="flex flex-col justify-center items-center p-6 md:p-8 lg:p-10 bg-black text-white min-h-screen">
+        <h1 className="text-base md:text-xl lg:text-3xl font-bold">My Portfolio</h1>
         <br />
-        <p className="text-xs md:text-sm lg:text-base text-center max-w-2xl">
+        <p className="text-base md:text-xl lg:text-2xl text-center max-w-4xl">
           This is my previous project and work. My portfolio covers several areas of programming languages, graphic design, code writing, digital marketing, and many more. It provides examples of work completed for clients and personal projects, offering insight into my skills and work style to help you in your upcoming endeavors.
         </p>
         
         {/* Button Section */}
-        <div className="flex justify-end mt-6 space-x-6 w-full max-w-4xl">
+        <div className="flex justify-end mt-6 space-x-6 w-full max-w-6xl">
           {["project", "profile"].map((section) => (
             <button
               key={section}
-              className={`px-3 py-2 text-sm md:text-base lg:text-xl font-semibold rounded-md shadow-md ${
+              className={`px-4 py-2 text-sm md:text-base lg:text-xl font-semibold rounded-md shadow-md ${
                 activeSection === section ? "bg-pink-600 shadow-lg" : "bg-pink-500 hover:bg-pink-600"
               }`}
               onClick={() => setActiveSection(section)}
@@ -44,7 +44,7 @@ export default function MyPortofolio() {
         </div>
         
         {/* Content Section */}
-        <div className="p-6 mt-10 bg-black shadow-xl shadow-white rounded-lg shadow-xl w-full max-w-4xl">
+        <div className="p-10 mt-10 bg-gray-100/20 shadow-xl rounded-lg w-full max-w-9xl text-white text-lg md:text-xl lg:text-2xl">
           {/* Conditional Rendering of Sections */}
           {activeSection === "project" && (
             <section id="project">
@@ -54,7 +54,6 @@ export default function MyPortofolio() {
 
           {activeSection === "profile" && (
             <section id="profile">
-              
               <AboutMe />
             </section>
           )}
